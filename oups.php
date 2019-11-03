062a781aafdc3d18120ab433b99d5e9e23da0bbc
Thomas <thomas@MacBook-Pro-de-Thomas.local>
2019-11-03 14:29:23 +0000

    nettoyage

--- oups.php
+++
@@ -1,5 +0,0 @@
-<link href="css/app.css" rel="stylesheet" >
-<div class="jumbotron">
-  <h1 class="display-3">OUPS</h1>
-  <p class="lead">web pages in workyard</p>  
-</div>
--- serveur.js
+++
@@ -1,5 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-   res.end('Robotize My Stuff !\n');
-}).listen(1337, '192.168.1.107');
