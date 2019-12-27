09c5555c18a9277f4d7e8d76edf62e99181f3466
Thomas <nastorgt62@gmail.com>
2019-12-27 12:20:20 +0100

    new

--- room/prise.js
+++
@@ -1,9 +0,0 @@
-
-const { Client } = require('index');
-
-const client = new Client();
-
-client.getDevice({ host: '192.168.1.45' }).then((device)=>{
-  device.getSysInfo().then(console.log);
-  device.setPowerState(true);
-});
