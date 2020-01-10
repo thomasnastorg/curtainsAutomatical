834197414129fccfa393bb1821166de1d115c0eb
Thomas <nastorgt62@gmail.com>
2020-01-10 22:48:25 +0000

    mise en page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css" ;
+import Home  from "./page/home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bonjour tout le monde bienvenu sur mon site d
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  return(
+    <div>
+      <header className= "App-header" >
+        <Home />
+        <h1>hello</h1>
       </header>
     </div>
   );
