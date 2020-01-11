4cf2a47c4ca4fb0b5f43559b50ad43ed157a7a0d
Thomas <nastorgt62@gmail.com>
2020-01-11 16:06:29 +0000

    dev

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css" ;
 import Home  from "./page/home";
+import Todo  from "./page/todo";
 
 function App() {
   return(
     <div>
       <header className= "App-header" >
-        <Home name="test"/>
+        <Home/>
         <h1>hello</h1>
       </header>
     </div>
--- src/page/home.js
+++ src/page/home.js
@@ -3,16 +3,13 @@ import React , { Component } from "react";
 
 
 class Home extends Component {
-  constructor() {
-    super();
-      this.state = {
-        cont = 0
-      }
-  }
-  
+
   render(){
     return(
-      <h1>kkkkk {this.props.name}</h1>
+      <div>
+        <h1>Home</h1>
+        
+      </div>
     );
   }
 }
