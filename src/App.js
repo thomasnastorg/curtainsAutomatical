d91aec1d826fd69619eab460611bbd90ef3d08e1
Thomas <nastorgt62@gmail.com>
2020-04-03 17:59:18 +0200

    chan

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css" ;
 import Home  from "./page/home";
 import Todo  from "./page/todo";
+import SingeIn  from "./page/singe_in";
 
 function App() {
   return(
+    /*
     <div>
       <header className= "App-header" >
-        <Home/>
-        <h1>hello</h1>
+        <SingeIn/>
       </header>
-    </div>
+    </div>*/
+
+    <Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}></Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>SingeIn</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={SingeIn} />
+            <Route path="/sign-in" component={SingeIn} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
   );
 }
 
