477803163261cf7a03c165621468cd5941e1379a
thomas <nastorgt62@gmail.com>
2020-04-05 21:42:29 +0200

    learing

--- src/App.js
+++ src/App.js
@@ -1,44 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import "./App.css" ;
-import Home  from "./page/home";
-import Todo  from "./page/todo";
-import SingeIn  from "./page/singe_in";
+import React, { Component } from 'react'
+import './App.css'
+import GuessCount from './GuessCount'
+import Card from './Card'
 
-function App() {
-  return(
-    /*
-    <div>
-      <header className= "App-header" >
-        <SingeIn/>
-      </header>
-    </div>*/
-
-    <Router>
-    <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div className="container">
-          <Link className="navbar-brand" to={"/sign-in"}></Link>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>SingeIn</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      <div className="auth-wrapper">
-        <div className="auth-inner">
-          <Switch>
-            <Route exact path='/' component={SingeIn} />
-            <Route path="/sign-in" component={SingeIn} />
-          </Switch>
-        </div>
+class App extends Component {
+  handleCardClicke(card){
+    console.log(card, 'clicked')
+  }
+  render() {
+    return (
+      <div className="memory"> 
+      <GuessCount guesses={0}/>
+      <Card card='#' feedback="hidden" onClick={this.handleCardClicke}/>
+      <Card card='@' feedback="justMatched"onClick={this.handleCardClicke}/>
+      <Card card='°' feedback="justMismatched"onClick={this.handleCardClicke}/>
+      <Card card='d' feedback="visble"onClick={this.handleCardClicke}/>
+      <Card card='w' feedback="hidden"onClick={this.handleCardClicke}/>
+      <Card card='<' feedback="justMatched"onClick={this.handleCardClicke}/>
       </div>
-    </div></Router>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
