a0c3bf74a3731cc527c2422ec608937a4c473e36
thomas <nastorgt62@gmail.com>
2020-04-09 18:24:52 +0200

    change

---
+++ src/Module.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+//creation de la card 
+class frame{
+    constructor(titre, largeur, hauteur){
+    this.titre = titre;
+    this.largeur = largeur;
+    this.hauteur = hauteur;
+    }
+}
+function heater(){
+
+
+}
+
+//cration titre chaufage 
+
+//titre rez de chaussée
+
+//insertion de la couleur pour la temperature
+
+//insertion de temperatur 
+
+
+
+
+
+
