3548e7c5b191cc323a9c6e3a6ef4dcdf1ed7a652
thomas <nastorgt62@gmail.com>
2020-08-10 16:24:51 +0200

    update

--- Mqtt/publisher.js
+++
@@ -1,9 +0,0 @@
-var mqtt = require('mqtt');
-var client = mqtt.connect('mqtt://192.168.1.105:1883');
-var topic = 'toto'
-client.on('connect', function () {
-    setInterval(function () {
-        client.publish(topic, 'Hello mqtt');
-        console.log('Message Sent');
-    }, 5000);
-});
